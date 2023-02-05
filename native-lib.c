b3310df3cc2e5358c27ca1879d039c574f7396f9
zq <zq@example.com>
2023-02-05 11:50:15 -0500

    update files

--- native-lib.c
+++ native-lib.c
@@ -8,21 +8,10 @@
 //    char str[] = "Hello from C ";
 //    return (*env)->NewStringUTF(env, str);
 //}
-
+// Our code implementation is based on the framework provided by John Schember
 /* Copyright (C) 2017 by John Schember <john@nachtimwald.com>
- *
- * Permission to use, copy, modify, and distribute this
- * software and its documentation for any purpose and without
- * fee is hereby granted, provided that the above copyright
- * notice appear in all copies and that both that copyright
- * notice and this permission notice appear in supporting
- * documentation, and that the name of M.I.T. not be used in
- * advertising or publicity pertaining to distribution of the
- * software without specific, written prior permission.
- * M.I.T. makes no representations about the suitability of
- * this software for any purpose.  It is provided "as is"
- * without express or implied warranty.
- */
+
+
 #include <netdb.h>
 #if defined(ANDROID) || defined(__ANDROID__)
 #include <time.h>
